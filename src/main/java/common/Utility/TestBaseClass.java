60759905b7225d793dbc17c62c125b8a4fe12e30
raghavhsp <raghavhsp@gmail.com>
2022-02-06 00:47:50 +0530

    Read Excel Data Chanages

--- src/main/java/common/Utility/TestBaseClass.java
+++ src/main/java/common/Utility/TestBaseClass.java
@@ -1,16 +1,12 @@
 package common.Utility;
 
-import java.io.File;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
